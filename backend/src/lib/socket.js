d5c1bfca561a67f5640ae64dac5d164746ffb48a
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-05-31 19:18:44 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -16,7 +16,7 @@ const userSocketMap = {};//{userId: socketId}
 // Create a Socket.IO server
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173']
+        origin: ['https://giggle-talk-codebase.vercel.app/']
     }
 });
 
